4680dba76c058b37f8c964a67e4cf87b4c803b37
R <rbeakes@DESKTOP-U67G7PO>
2023-05-07 19:16:06 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='edgarl',
+    version='0.1',
+    description='EDGAR Local DB',
+    packages=find_packages(),
+)
