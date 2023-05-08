7c63c28bef1e34655290785c9aa835c1a25947e8
R <rbeakes@DESKTOP-U67G7PO>
2023-05-07 22:30:18 -0400

    Fixes and lookup function

--- egdarl/__init__.py
+++ egdarl/__init__.py
@@ -15,26 +15,26 @@ HEADERS = {"User-Agent": "Mozilla/5.0"}
 
 
 class EDGAR:
-    def __init__(self, dir: str):
-        self.dir = dir
-        self._company_facts = os.path.join(self.dir, 'company_facts')
-        self._submissions = os.path.join(self.dir, 'submissions')
-        self._summaries = os.path.join(self.dir, 'summaries')
+    def __init__(self, path: str):
+        self.path = path
+        self._company_facts = os.path.join(self.path, 'company_facts')
+        self._submissions = os.path.join(self.path, 'submissions')
+        self._summaries = os.path.join(self.path, 'summaries')
 
-        for dir in [self.dir, self._company_facts, self._submissions, self._summaries]:
-            os.makedirs(dir, exist_ok=True)
+        for d in [self.path, self._company_facts, self._submissions, self._summaries]:
+            os.makedirs(d, exist_ok=True)
 
-        self._ticker_cik_map_path = os.path.join(self.dir, 'ticker.json')
+        self._ticker_cik_map_path = os.path.join(self.path, 'ticker.json')
         self._ticker_cik_map = json.load(open(self._ticker_cik_map_path)) if os.path.isfile(self._ticker_cik_map_path) else {}
 
-        self._company_cik_map_path = os.path.join(self.dir, 'ticker.json')
+        self._company_cik_map_path = os.path.join(self.path, 'ticker.json')
         self._company_cik_map = json.load(open(self._company_cik_map_path)) if os.path.isfile(self._company_cik_map_path) else {}
 
     def build(self):
         now = datetime.now()
-        summary_path = os.path.join(self.dir, 'summary.json')
-        company_zip = os.path.join(self.dir, 'company_facts.zip')
-        submission_zip = os.path.join(self.dir, 'submissions.zip')
+        summary_path = os.path.join(self.path, 'summary.json')
+        company_zip = os.path.join(self.path, 'company_facts.zip')
+        submission_zip = os.path.join(self.path, 'submissions.zip')
 
         if os.path.isfile(summary_path):
             last_summary = json.load(open(summary_path))
@@ -45,7 +45,7 @@ class EDGAR:
 
         for file_name in os.listdir(self._submissions):
             if '.json' in file_name:
-                with open(os.path.join(self.dir, file_name), 'r') as file:
+                with open(os.path.join(self.path, file_name), 'r') as file:
                     try:
                         submission = json.load(file)
                         cik = submission['cik']
@@ -65,7 +65,7 @@ class EDGAR:
         with open(summary_path, 'w+') as summary_file:
             summary = {
                 'build_timestamp': now.isoformat(),
-                'total_size': sum(d.stat().st_size for d in os.scandir(self.dir) if d.is_file()),
+                'total_size': sum(d.stat().st_size for d in os.scandir(self.path) if d.is_file()),
                 'company_facts': {
                     'size': sum(d.stat().st_size for d in os.scandir(self._company_facts) if d.is_file()),
                     'files': len(os.listdir(self._company_facts)),
@@ -79,14 +79,20 @@ class EDGAR:
             }
             summary_file.write(json.dumps(summary, indent=4))
 
+    def lookup_cik(self, ticker=None, company_name=None):
+        if ticker and ticker in self._ticker_cik_map:
+            return self._ticker_cik_map[ticker]
+        if company_name and company_name in self._company_cik_map:
+            return self._company_cik_map[company_name]
 
-def process_zip(url, path, dir):
+
+def process_zip(url, zip_path, folder_path):
     with requests.get(url, headers=HEADERS, stream=True) as r:
         r.raise_for_status()
-        with open(path, 'wb+') as file:
+        with open(zip_path, 'wb+') as file:
             for chunk in r.iter_content():
                 file.write(chunk)
 
-    if len(os.listdir(dir)) <= 0:
-        with ZipFile(path, 'r') as zip:
-            zip.extractall(dir)
+    if len(os.listdir(folder_path)) <= 0:
+        with ZipFile(zip_path, 'r') as file:
+            file.extractall(folder_path)
